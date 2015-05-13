f4fcc36cabdc94f7e3de62fab383f3e4fad6b18f
mike <github@ravuya.com>
2015-05-13 09:57:00 -0600

    can now configure what blog to target.

--- tumblrposter.py
+++ tumblrposter.py
@@ -1,12 +1,32 @@
 import pytumblr
 import yaml
+import sys
 
 def load_secrets(secretsfile):
 	with open(secretsfile) as stream:
 		return yaml.load(stream)
 
-secrets = load_secrets('secrets.yml')
+def assert_secrets(secrets):
+	for mandatory_key in ['consumer_key', 'consumer_secret', 'token_key', 'token_secret']:
+		if mandatory_key not in secrets:
+			print 'Missing mandatory secrets.yml key called', mandatory_key
+			sys.exit()
 
+def load_config():
+	with open('config.yml') as stream:
+		return yaml.load(stream)
+
+def tryget_config(config, key, fallback_value = None):
+	if key not in config:
+		return fallback_value
+	return config[key]
+
+secrets = load_secrets('secrets.yml')
+assert_secrets(secrets)
+
+config = load_config()
+
+# auth via oauth
 client = pytumblr.TumblrRestClient(
 		secrets['consumer_key'],
 		secrets['consumer_secret'],
@@ -14,4 +34,8 @@ client = pytumblr.TumblrRestClient(
 		secrets['token_secret']
 	 )
 
-print client.info()
+info = client.info()
+blogs = info['user']['blogs']
+
+blog_sought = tryget_config(config, 'blog', blogs[0]['name'])
+print "Targeting blog:", blog_sought
